73508e53b699346018b82e9422205c79dd03a638
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-03-12 00:34:13 +0200

    No need for stream constructor, as stream.Transform is totally awesome.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var gaze    = require('gaze'),
     path    = require('path'),
     mkdirp  = require('mkdirp'),
     glob    = require('glob'),
-    through = require('through'),
     Q       = require('q');
 
 exports.list = function(pattern, cb) {
@@ -14,7 +13,8 @@ exports.watch = function(pattern, cb) {
   Q.nfcall(gaze, pattern)
   .then(function(watcher) {
     watcher.on('all', function(event, file) {
-      // Absolute to relative path
+
+      // Convert from absolute to relative path
       cb(event, path.relative(process.cwd(), file));
     });
   })
@@ -29,20 +29,3 @@ exports.write = function(file) {
   mkdirp.sync(path.dirname(file));
   return fs.createWriteStream(file);
 }
-
-exports.transformer = function(transform) {
-  return (function(data){
-    return through(function write(chunk){
-      data += chunk;
-    },
-    function end() {
-      var stream = this;
-      Q.when(transform(data))
-        .then(function(res) {
-          stream.queue(res);
-          stream.queue(null);
-        })
-        .done()
-    });
-  })('')
-};
