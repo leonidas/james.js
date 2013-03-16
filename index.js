59006a0a2a00ba4649a44a0f54f6f16f52ab05ee
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-03-16 10:25:59 +0200

    Better error handling.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var gaze     = require('gaze'),
     glob     = require('glob'),
     Q        = require('q'),
     stream   = require('readable-stream'), // For node.js 0.8.x support
+    colors   = require('colors'),
     Pipeline = require('./lib/pipeline'),
+    _        = require('underscore'),
     tasks    = {};
 
 exports.task = function(name, task) {
@@ -24,7 +26,12 @@ exports.run = function(name) {
     task();
   }
   else {
-    console.log('Error: Task "' + name + '" not found in Jamesfile.');
+    console.error(('Error: Task "' + name + '" not found in Jamesfile.').red)
+    console.log('Available tasks: ');
+    tasks = _.keys(tasks);
+    for (var i = 0; i < tasks.length; i++) {
+      console.log('"' + tasks[i] + '"');
+    };
   }
 }
 
@@ -51,30 +58,48 @@ exports.read = function(stream) {
   return new Pipeline(stream);
 }
 
-exports.write = function(file) {
-  mkdirp.sync(path.dirname(file));
-  return fs.createWriteStream(file);
+exports.write = function(filename) {
+  // Write first to pass through buffer in order to avoid partial files written
+  // in case of errors.
+  buf = new stream.PassThrough();
+
+  buf.on('finish', function() {
+    mkdirp.sync(path.dirname(filename));
+    file = fs.createWriteStream(filename);
+    this.pipe(file);
+    file.on('close', function() { console.log('Wrote file '.green + filename.green) });
+  });
+  return buf;
 }
 
 _transform = function(chunk, encoding, callback) {
-  this._file += chunk;
+  this._content += chunk;
   callback();
 }
 
 _flush = function(f) {
   return function(callback) {
     var self = this;
-    f(this._file, function(result) {
-      self.push(result);
-      callback();
-    });
+    try {
+      f(this._content, function(result) {
+        self.push(result);
+        callback();
+      });
+
+    } catch (err) {
+      callback(err);
+    }
   }
 }
 
 exports.createStream = function(f) {
   var s = new stream.Transform();
-  s._file      = '';
+  s._content   = '';
   s._transform = _transform;
   s._flush     = _flush(f);
+  s.on('error', function(err){
+    console.error("ERROR:".red, err.message.red);
+    process.exit(1);
+  });
   return s;
 }
